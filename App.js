f343c8b6f4fcb120d000e2fe7f53a77fd2b1701c
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-05-21 11:50:36 +0200

    MenuScreen Impl

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -7,55 +7,20 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { Button as Button1 } from "react-native-elements";
 
 import { StartScreen } from "./src/screens/StartScreen";
+import { MenuScreen } from "./src/screens/MenuScreen";
 
-class Menu extends React.Component {
-  handleClick = () => {
-    console.log("this is :", this);
-  };
-
-  render() {
-    return (
-      <View style={styles.fixBackground}>
-        <View style={styles.container}>
-          <Text style={{ fontSize: 33, marginTop: 50 }}>RPS Jakob-Yazan</Text>
-        </View>
-        <View style={styles.box}>
-          <Button1
-            title="Spela Online"
-            style={{ width: 150, marginTop: 150 }}
-            type={"outline"}
-          />
-          <Button1
-            title=" Spela Dator"
-            style={{ width: 150, marginTop: 20 }}
-            type={"outline"}
-            onPress={this.handleClick}
-          />
-        </View>
-
-        <View style={styles.buttomRow}>
-          <Button1
-            title="Hem"
-            size={15}
-            style={{ height: 50, width: 70, marginTop: 20 }}
-            type={"outline"}
-          />
-          <Button1
-            title="Spel"
-            size={15}
-            style={{ height: 50, width: 70, marginTop: 20 }}
-            type={"outline"}
-          />
-          <Button1
-            title="Lista"
-            size={15}
-            style={{ height: 50, width: 70, marginTop: 20 }}
-            type={"outline"}
-          />
-        </View>
-      </View>
-    );
-  }
+function Menu() {
+  return (
+    <>
+      <MenuScreen />
+      {/* 
+      
+      onPress till alla knappar ska in här
+      
+      
+      */}
+    </>
+  );
 }
 
 function Start({ navigation }) {
@@ -98,23 +63,6 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
-  },
-  fixBackground: {
-    backgroundColor: "#fff",
-  },
-  row: {
-    flexDirection: "row",
-  },
-  box: {
-    alignItems: "center",
-    alignContent: "center",
-    justifyContent: "center",
-    flexDirection: "column",
-  },
-  buttomRow: {
-    flexDirection: "row",
-    marginTop: 120,
     justifyContent: "center",
   },
 });
---
+++ src/screens/MenuScreen.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import { Button as Button1 } from "react-native-elements";
+
+export function MenuScreen() {
+  return (
+    <View style={styles.fixBackground}>
+      <View style={styles.container}>
+        <Text style={{ fontSize: 33, marginTop: 50 }}>RPS Jakob-Yazan</Text>
+      </View>
+      <View style={styles.box}>
+        <Button1
+          title="Spela Online"
+          style={{ width: 150, marginTop: 150 }}
+          type={"outline"}
+        />
+        <Button1
+          title=" Spela Dator"
+          style={{ width: 150, marginTop: 20 }}
+          type={"outline"}
+        />
+      </View>
+
+      <View style={styles.buttomRow}>
+        <Button1
+          title="Hem"
+          size={15}
+          style={{ height: 50, width: 70, marginTop: 20 }}
+          type={"outline"}
+        />
+        <Button1
+          title="Spel"
+          size={15}
+          style={{ height: 50, width: 70, marginTop: 20 }}
+          type={"outline"}
+        />
+        <Button1
+          title="Lista"
+          size={15}
+          style={{ height: 50, width: 70, marginTop: 20 }}
+          type={"outline"}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  fixBackground: {
+    backgroundColor: "#fff",
+  },
+  row: {
+    flexDirection: "row",
+  },
+  box: {
+    alignItems: "center",
+    alignContent: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  buttomRow: {
+    flexDirection: "row",
+    marginTop: 120,
+    justifyContent: "center",
+  },
+});
