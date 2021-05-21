18b05fd13909dc26c19838e869b9a5b12a2969b3
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-05-21 20:39:04 +0200

    Impl WinnerScreen

---
+++ src/screens/WinnerScreen.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+import { Button } from "react-native-elements";
+
+export function WinnerScreen() {
+  return (
+    <View style={styles.fixBackground}>
+      <View style={styles.container}>
+        <Text style={{ fontSize: 33, marginTop: 50 }}>Yazan vann med: </Text>
+        <Image style={styles.pic} source={require("../../assets/rock.gif")} />
+      </View>
+
+      <View style={styles.buttomRow}>
+        <Button
+          title="Spela Igen"
+          size={25}
+          style={{ height: 70, width: 70, marginTop: 20, marginRight: 20 }}
+          type={"outline"}
+        />
+        <Button
+          title="Avlsuta Spel"
+          size={15}
+          style={{ height: 70, width: 70, marginTop: 20, marginRight: 20 }}
+          type={"outline"}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  fixBackground: {
+    backgroundColor: "#fff",
+  },
+  row: {
+    flexDirection: "row",
+  },
+  box: {
+    alignItems: "center",
+    alignContent: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  buttomRow: {
+    flexDirection: "row",
+    marginTop: 120,
+    justifyContent: "center",
+    marginBottom: 250,
+    marginLeft: 20,
+  },
+  pic: {
+    width: 200,
+    height: 200,
+    marginTop: 40,
+  },
+});
