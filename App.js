c71f81d21aa5f023025f7a3bad746de5a0164fe2
yazan <alkhouriyazan@gmail.com>
2021-05-27 20:28:21 +0200

    App update

--- App.js
+++ App.js
@@ -1,33 +1,56 @@
-import React, { useState, useEffect } from "react";
-import {
-  StyleSheet,
-  View,
-  ActivityIndicator,
-  FlatList,
-  Text,
-} from "react-native";
+import React, { useState,useEffect, createContext } from "react";
+import { StyleSheet, View, ActivityIndicator, FlatList, Text } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import { GetListApi } from "./src/Context/GetListApi";
+import { StartScreen } from "./src/screens/StartScreen";
+import { MenuScreen } from "./src/screens/MenuScreen";
+import { InGameScreen } from "./src/screens/InGameScreen";
+import { WinnerScreen } from "./src/screens/WinnerScreen";
+import { PcWinnerScreen } from "./src/screens/PcWinnerScreen";
 
-import { StartScreen } from "./src/Screens/StartScreen";
-import { MenuScreen } from "./src/Screens/MenuScreen";
-import { InGameScreen } from "./src/Screens/InGameScreen";
-import { WinnerScreen } from "./src/Screens/WinnerScreen";
-import { PcWinnerScreen } from "./src/Screens/PcWinnerScreen";
 
-import { ListGamesScreen } from "./src/Screens/ListGamesScreen";
-import { JoinablePlayersScreen } from "./src/Screens/JoinablePlayersScreen";
+
+import { ListGamesScreen } from "./src/screens/ListGamesScreen";
+import { JoinablePlayersScreen } from "./src/screens/JoinablePlayersScreen";
 
 import AppContext from "./src/Context/AppContext";
 
 function Start() {
+  // const [isLoading, setLoading] = useState(true);
+  // const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('https://reactnative.dev/movies.json',
+  //   {
+  //   method: 'Get',
+  //   headers: {
+  //   Accept: 'application/json',
+  //   // Token: "",
+  //   'Content-Type': 'application/json'
+  // }}, 
+  //   )
+    
+  //     .then((response) => response.json())
+  //     .then((json) => setData(json.movies))
+  //     .catch((error) => console.error(error))
+  //     .finally(() => setLoading(false));
+  // }, []);
   return (
     <View style={styles.container}>
       <StartScreen />
-      <GetListApi />
+
+      {/* {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.title}, {item.releaseYear}</Text>
+          )}
+        />
+      )} */}
+     
     </View>
   );
 }
@@ -88,16 +111,31 @@ function OfflineWinner() {
 }
 const Stack = createStackNavigator();
 
+const TokenContex = createContext();
+
 export default function App() {
+
+  const [token, setToken] = useState("");
+  console.log("token1",token)
+  useEffect(() => {
+        fetch('http://localhost:8080/tokens/new')
+        .then(response => response.text())
+        .then(newToken => {setToken(newToken); console.log("token",token)});
+        
+      }, );[]
+
+      console.log("token2",token)
+
   const [nickName, setNickName] = useState("Jakob vs ...");
 
+
   return (
+    <TokenContex.Provider value={token}>
     <AppContext.Provider value={nickName}>
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Start" component={Start} />
           <Stack.Screen name="Menu" component={Menu} />
-
           <Stack.Screen name="Offline" component={Offline} />
           <Stack.Screen name="Online" component={Online} />
 
@@ -109,6 +147,7 @@ export default function App() {
         </Stack.Navigator>
       </NavigationContainer>
     </AppContext.Provider>
+    </TokenContex.Provider>
   );
 }
 
