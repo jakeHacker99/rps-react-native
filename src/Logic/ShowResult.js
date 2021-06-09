0fc605b38361c046b76c025773dd99ac84478350
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-06-09 18:45:37 +0200

    ShowResult.js clean

--- src/Logic/ShowResult.js
+++ src/Logic/ShowResult.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { Text, Image, StyleSheet, View } from "react-native";
+
 import AppContext from "../Context/AppContext";
 
 export default function ShowResult() {
@@ -32,39 +33,16 @@ export default function ShowResult() {
   );
 }
 const styles = StyleSheet.create({
-  container: {},
-  fixBackground: {
-    backgroundColor: "#fff",
-    marginTop: 50,
-  },
-  row: {
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column",
-  },
   box: {
     alignItems: "center",
     alignContent: "center",
     justifyContent: "center",
     flexDirection: "column",
   },
-  buttomRow: {
-    flexDirection: "row",
-    marginTop: 120,
-    justifyContent: "center",
-    marginBottom: 250,
-    marginLeft: 20,
-  },
+
   pic: {
     width: 200,
     height: 200,
     marginTop: 40,
-  },
-  styleButton: {
-    height: 70,
-    width: 70,
-    marginTop: 20,
-    marginRight: 20,
   },
 });
